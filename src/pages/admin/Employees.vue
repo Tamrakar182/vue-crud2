03cd6c4947b2604cca060df9c24372833669e868
Manjul <manjultamrakar4@gmail.com>
2022-10-24 17:23:40 +0545

    changed indentation

--- src/pages/admin/Employees.vue
+++ src/pages/admin/Employees.vue
@@ -2,26 +2,26 @@
   <router-link :to="{name:'EmployeeCreate'}" class="btn">Add Employee</router-link>
 
   <table>
-    <thead>
-      <tr>
-        <th>#id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Role</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="employee in employees" :key="employee.id">
-        <td>{{employee.id}}</td>
-        <td>{{employee.Name}}</td>
-        <td>{{employee.Address}}</td>
-        <td>{{employee.Role}}</td>
-        <td>
-          <button @click="del(employee.id)" class="btn btn-del">Delete</button>
-        </td>
-      </tr>
-    </tbody>
+	<thead>
+	  <tr>
+		<th>#id</th>
+		<th>Name</th>
+		<th>Address</th>
+		<th>Role</th>
+		<th>Actions</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  <tr v-for="employee in employees" :key="employee.id">
+		<td>{{employee.id}}</td>
+		<td>{{employee.Name}}</td>
+		<td>{{employee.Address}}</td>
+		<td>{{employee.Role}}</td>
+		<td>
+		  <button @click="del(employee.id)" class="btn btn-del">Delete</button>
+		</td>
+	  </tr>
+	</tbody>
   </table>
 </template>
 
@@ -32,22 +32,22 @@ import { onMounted } from '@vue/runtime-core';
 export default {
   name:'Employees',
   setup() {
-    const employees = ref([]);
+	const employees = ref([]);
 
-    onMounted ( async () => {
-      const res = await fetch('http://localhost:3000/employees');
-      employees.value = await res.json();
-    })
+	onMounted ( async () => {
+	  const res = await fetch('http://localhost:3000/employees');
+	  employees.value = await res.json();
+	})
 
-    const del = async (id) => {
-      await fetch(`http://localhost:3000/employees/${id}`, {
-        method:'DELETE'
-      });
+	const del = async (id) => {
+	  await fetch(`http://localhost:3000/employees/${id}`, {
+		method:'DELETE'
+	  });
 
-      employees.value = employees.value.filter( p => p.id !== id);
-    }
+	  employees.value = employees.value.filter( p => p.id !== id);
+	}
 
-    return { employees, del }
+	return { employees, del }
   }
 }
 </script>
